ac019de8f923c1f3bac9af3e8cecc044636b4417
paprende <prendergast2k@hotmail.co.uk>
2025-12-15 12:53:41 +0700

    chore: add testing

--- src/api/pokemonClient.ts
+++ src/api/pokemonClient.ts
@@ -13,7 +13,7 @@ export interface Pokemon {
       "official-artwork"?: {
         front_default?: string | null;
       };
-    };
+    };``
   };
   types: { slot: number; type: { name: string; url: string } }[];
   abilities: { ability: { name: string; url: string }; is_hidden: boolean }[];
