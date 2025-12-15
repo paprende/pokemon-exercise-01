02b520c7aca81731ad7e8fc9ce5896850ef35023
paprende <prendergast2k@hotmail.co.uk>
2025-12-15 13:00:55 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p>
-        THIS IS YOUR APP 😊
-      </p>
-    </>
+    <div className="container py-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card shadow">
+            <div className="card-body text-center">
+              <h1 className="card-title mb-4">THIS IS YOUR APP 😊</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
