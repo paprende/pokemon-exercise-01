e998fd663849cf83a8ba03f49058698794afefc9
paprende <prendergast2k@hotmail.co.uk>
2026-02-09 13:23:27 +0700

    update

--- src/components/pokemon-app.tsx
+++ src/components/pokemon-app.tsx
@@ -1,9 +1,11 @@
 function PokemonApp() {
-    return (<div className="card shadow">
-        <div className="card-body text-center">
-            <h1 className="card-title mb-4">THIS IS YOUR APP 😊</h1>
-        </div>
-    </div>)
+  return (
+    <div className="card shadow">
+      <div className="card-body text-center">
+        <h1 className="card-title mb-4">THIS IS YOUR APP 😊</h1>
+      </div>
+    </div>
+  )
 }
 
 export default PokemonApp;
